603460748a9338f2c84069025d9335d6de92d8b6
Erick Ortiz <108337650+VeNoNs@users.noreply.github.com>
2025-05-19 13:06:47 -0500

    Creation Models
    creation of the user and credentials models

---
+++ service-user/src/main/java/model/Credential.java
@@ -0,0 +1,33 @@
+package com.bank.serviceuser.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "credenciales")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Credential {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_credencial")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
+    private User user;
+
+    @Column(name = "hash_contrasena")
+    private String passwordHash;
+
+    @Column(name = "dos_factores_activado")
+    private Boolean dosFactoresActivado;
+
+    @Column(name = "ultimo_login")
+    private LocalDateTime ultimoLogin;
+}
