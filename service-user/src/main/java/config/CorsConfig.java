79278e964825113e85cb57b80ef032bfb199641a
Erick Ortiz <108337650+VeNoNs@users.noreply.github.com>
2025-05-19 13:42:13 -0500

    Config apirest
    config

---
+++ service-user/src/main/java/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.bank.serviceuser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todas las rutas
+                .allowedOrigins("*") // Permitir desde cualquier origen (ajustable)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
