e691664cef6b71c53ebdf33ca5952029aeb13cdc
AimarPriCu <U22234170@utp.edu.pe>
2025-05-24 10:58:53 -0500

    Creacion clase model

---
+++ service-fraud/src/main/java/com/bank/service_fraud/model/TipoTransaccionAlerta.java
@@ -0,0 +1,19 @@
+package com.bank.service_fraud.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tipos_transaccion_alerta")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TipoTransaccionAlerta {
+
+    @Id
+    @Column(name = "id_tipo")
+    private Integer id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String nombre;
+}
