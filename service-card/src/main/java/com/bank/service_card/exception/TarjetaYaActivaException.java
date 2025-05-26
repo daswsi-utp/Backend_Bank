679f775a02a2fc9762ddfea62caa42fdf3d85774
rtp-robinson <105247920+rtp-robinson@users.noreply.github.com>
2025-05-26 13:16:53 -0500

    global error handling and custom exceptions

---
+++ service-card/src/main/java/com/bank/service_card/exception/TarjetaYaActivaException.java
@@ -0,0 +1,8 @@
+
+package com.bank.service_card.exception;
+
+public class TarjetaYaActivaException extends RuntimeException {
+    public TarjetaYaActivaException(String message) {
+        super(message);
+    }
+}
